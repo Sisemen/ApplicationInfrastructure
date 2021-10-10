dc87a14d1fe78e721caf8dfdd0bdb9724a3afb98
Ali Khaleghifard <ali.khaleghifard@tuni.fi>
2021-10-10 03:29:59 +0300

    Indentation

--- Core/Kernel/DataAccess/Model/Entity.cs
+++ Core/Kernel/DataAccess/Model/Entity.cs
@@ -7,7 +7,8 @@ namespace Core.Kernel.DataAccess.Model
 {
     public abstract class Entity<T> : IEntity
     {
-        [Key] public T Id { get; set; }
+        [Key]
+        public T Id { get; set; }
         public byte[] Version { get; private set; }
 
         byte[] IEntity.Version
@@ -16,7 +17,9 @@ namespace Core.Kernel.DataAccess.Model
             set => this.Version = value;
         }
 
-        [NotMapped] public T TemporaryId { get; set; }
-        [NotMapped] public abstract Action<ModelBuilder> OnConfiguringEntity { get; }
+        [NotMapped]
+        public T TemporaryId { get; set; }
+        [NotMapped]
+        public abstract Action<ModelBuilder> OnConfiguringEntity { get; }
     }
 }
