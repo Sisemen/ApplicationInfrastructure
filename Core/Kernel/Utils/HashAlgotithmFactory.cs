110172fe637c3ab97f52c2ac91efa096c8b1f02c
Ali Khaleghifard <33933819+Sisemen@users.noreply.github.com>
2024-12-11 15:03:53 +0200

    HashUtil has been added.

---
+++ Core/Kernel/Utils/HashAlgotithmFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Authentication;
+using System.Security.Cryptography;
+
+namespace Core.Kernel.Utils
+{
+    public class HashAlgorithmFactory
+    {
+        public static HashAlgorithm Create(HashAlgorithmType type)
+        {
+            switch (type)
+            {
+                case HashAlgorithmType.Md5:
+                    return MD5.Create();
+                case HashAlgorithmType.Sha1:
+                    return SHA1.Create();
+                case HashAlgorithmType.Sha256:
+                    return SHA256.Create();
+                case HashAlgorithmType.Sha384:
+                    return SHA384.Create();
+                case HashAlgorithmType.Sha512:
+                    return SHA512.Create();
+                default:
+                    throw new ArgumentException("Unsupported hash algorithm type.");
+            }
+        }
+    }
+}
