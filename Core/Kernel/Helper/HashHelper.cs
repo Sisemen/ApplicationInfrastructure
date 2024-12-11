aafb2dbe7cd0baf3a8d527134afe89801ad39bb5
Ali Khaleghifard <alikhaleghifard@gmail.com>
2024-12-11 02:18:15 +0200

    HashHelper has been added.

---
+++ Core/Kernel/Helper/HashHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Core.Kernel.Helper
+{
+    public static class HashHelper
+    {
+        public static string? Encode(this byte[]? data) => data is null ? null : Convert.ToBase64String(data);
+        public static byte[]? Decode(this string? data) => string.IsNullOrWhiteSpace(data) ? null : Convert.FromBase64String(data);
+    }
+}
