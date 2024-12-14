5ea6a3ada4236f3cdfcee24067c14b896ecdcfa8
Ali Khaleghifard <33933819+Sisemen@users.noreply.github.com>
2024-12-14 02:43:43 +0200

    Service handler interface has been updated.

--- Core/Kernel/Service/IServiceHandler.cs
+++ Core/Kernel/Service/IServiceHandler.cs
@@ -1,8 +1,11 @@
 ﻿using Core.Kernel.Dependency;
+using System.Threading.Tasks;
+using System;
 
 namespace Core.Kernel.Service
 {
     public interface IServiceHandler : IPerLifetimeScopeDependencyInjection
     {
+        Task<IDto?> HandleAsync<TResponse>(Func<Task<IDto?>> serviceHandlerDelegate);
     }
 }
